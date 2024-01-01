6bc400244d281ad45e518a8c54112e0036371e61
JonPlaytv <gamerjon00@gmail.com>
2024-01-01 18:11:24 +0100

    fresh

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ def index():
 @app.route('/generate', methods=['POST'])
 def generate():
     # Get user input from the request
-    user_text = request.json.get('prompt')  # assuming your request structure has 'prompt'
+    user_text = request.json.get('prompt')
+    width = request.json.get('width')  # Assuming your request structure includes 'width'
+    height = request.json.get('height')  # Assuming your request structure includes 'height'
 
     # Make a request to your FastAPI endpoint
-    response = requests.post('http://37.60.173.43:8080/sdapi/v1/txt2img', json={'text': user_text})
+    response = requests.post('http://37.60.173.43:8080/sdapi/v1/txt2img', json={'text': user_text, 'width': width, 'height': height})
 
     # Assume the API responds with an image encoded in base64
     base64_image = response.json().get('images')
-    print(response.text)
+
     return jsonify({'images': base64_image})
 
 @app.route('/queue/status')
